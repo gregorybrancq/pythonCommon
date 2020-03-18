0371bbd8dc0bf8d15aa5185ed458b5fb2c017c03
Gregory Brancq <gregory.brancq@free.fr>
2020-03-18 22:25:17 +0100

    Moved mail function in another file

--- basic.py
+++ basic.py
@@ -146,13 +146,6 @@ def move_files(old_path, new_path):
             pass
 
 
-
-##############################################
-##############################################
-#            ADVANCED FUNCTIONS             ##
-##############################################
-##############################################
-
 def addFile(log, header, fileName, extAuth, typeList, warnNb):
     log.info(header, "In  addFile fileName=" + str(fileName))
     dirN = os.path.dirname(fileName)
@@ -191,45 +184,3 @@ def listFromArgs(log, header, args, ext):
 
     log.info(header, "Out listFromArgs typeList=" + str(typeList))
     return typeList, warnNb
-
-
-def sendMail(From, To, Cc, Subject, MessageText, MessageHtml):
-    # Import smtplib for the actual sending function
-    import smtplib
-    # Import the email modules we'll need
-    from cStringIO import StringIO
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    from email.header import Header
-    from email.charset import Charset
-    from email.generator import Generator
-
-    # Default encoding mode set to Quoted Printable. Acts globally!
-    Charset.add_charset('utf-8', Charset.QP, Charset.QP, 'utf-8')
-    # 'alternative’ MIME type – HTML and plain text bundled in one e-mail message
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = Header(Subject, 'utf-8')
-    # Only descriptive part of recipient and sender shall be encoded, not the email address
-    # msg['From'] = Header(From, 'utf-8')
-    msg['From'] = From
-    msg['To'] = To
-    msg['Cc'] = Cc
-
-    # Attach the parts with the given encodings.
-    htmlpart = MIMEText(MessageHtml, 'html', 'UTF-8')
-    msg.attach(htmlpart)
-    textpart = MIMEText(MessageText, 'plain', 'UTF-8')
-    msg.attach(textpart)
-
-    # And here we have to instantiate a Generator object to convert the msg
-    # object to a string (can't use msg.as_string, because that escapes
-    # "From" lines).
-    io = StringIO()
-    g = Generator(io, False)  # second argument means "should I mangle From?"
-    g.flatten(msg)
-
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
-    s = smtplib.SMTP('localhost')
-    s.sendmail(From, [To], io.getvalue())
-    s.quit()
