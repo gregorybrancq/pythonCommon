e5f2ef2435fbeb7652896bcd70d02e35c682fc1b
Gregory Brancq <gregory.brancq@free.fr>
2020-04-28 21:05:21 +0200

    Gtk dialog in python3.
    Change-Id: I40b8c7a57f0cbb1c2fda7ad31201eb4caef1bab9

--- message.py
+++ message.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# basic import
-import sys
+"""
+Open a window to print the status of the program
+"""
 
 # use for graphical interface
-try:
-    import pygtk
-    pygtk.require('2.0')
-except ImportError as e:
-    sys.exit("Issue with PyGTK.\n" + str(e))
-try:
-    import gtk
-except (RuntimeError, ImportError) as e:
-    sys.exit("Issue with GTK.\n" + str(e))
+import gi
 
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-###############################################
-## Message Dialog Class
-###############################################
+##############################################
+# Message Dialog Class
+##############################################
 
 """Shows a message. The message type, title and the message to be
 displayed can be passed when initializing the class."""
@@ -28,50 +23,82 @@ class MessageDialog(object):
     """Shows a message. The message type, title and the message to be
     displayed can be passed when initializing the class."""
 
-    def __init__(self, type_, title, message):
-        self.type = type_
+    def __init__(self, dialog_type, title, message1, message2=None):
+        self.type = dialog_type
+
         if self.type == 'error':
-            self.dialog = gtk.MessageDialog(
-                type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_CLOSE)
+            self.dialog = Gtk.MessageDialog(Gtk.Window(),
+                                            Gtk.DialogFlags.MODAL,
+                                            Gtk.MessageType.ERROR,
+                                            Gtk.ButtonsType.CLOSE,
+                                            message1,
+                                            )
+            if message2 is not None:
+                self.dialog.format_secondary_text(message2)
+
         elif self.type == 'info':
-            self.dialog = gtk.MessageDialog(
-                type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_CLOSE)
+            self.dialog = Gtk.MessageDialog(Gtk.Window(),
+                                            Gtk.DialogFlags.MODAL,
+                                            Gtk.MessageType.INFO,
+                                            Gtk.ButtonsType.CLOSE,
+                                            message1,
+                                            )
+            if message2 is not None:
+                self.dialog.format_secondary_text(message2)
+
         elif self.type == 'question':
-            self.dialog = gtk.MessageDialog(
-                type=gtk.MESSAGE_QUESTION, buttons=gtk.BUTTONS_YES_NO)
+            self.dialog = Gtk.MessageDialog(Gtk.Window(),
+                                            Gtk.DialogFlags.MODAL,
+                                            Gtk.MessageType.QUESTION,
+                                            Gtk.ButtonsType.YES_NO,
+                                            message1,
+                                            )
+            if message2 is not None:
+                self.dialog.format_secondary_text(message2)
+
         elif self.type == 'entry':
-            self.dialog = gtk.MessageDialog(
-                type=gtk.MESSAGE_QUESTION, buttons=gtk.BUTTONS_YES_NO)
-            dialogBox = self.dialog.get_content_area()
-            self.userEntry = gtk.Entry()
+            self.dialog = Gtk.MessageDialog(Gtk.Window(),
+                                            Gtk.DialogFlags.MODAL,
+                                            Gtk.MessageType.QUESTION,
+                                            Gtk.ButtonsType.YES_NO,
+                                            message1,
+                                            )
+            if message2 is not None:
+                self.dialog.format_secondary_text(message2)
+
+            dialog_box = self.dialog.get_content_area()
+            self.userEntry = Gtk.Entry()
             self.userEntry.show()
             # the following will trigger OK response when enter is hit in the entry
-            self.userEntry.connect("activate", lambda w: self.dialog.response(gtk.RESPONSE_YES))
-            dialogBox.pack_end(self.userEntry, False, False, 0)
+            self.userEntry.connect("activate", lambda w: self.dialog.response(Gtk.ResponseType.YES))
+            dialog_box.pack_end(self.userEntry, False, False, 0)
+
         self.dialog.set_title(title)
-        self.dialog.set_markup(message)
         self.dialog.show_all()
 
     def run(self):
         """Runs the dialog and closes it afterwards."""
         response = self.dialog.run()
-        self.dialog.hide()
-        if (response == gtk.RESPONSE_YES) and (self.type == 'entry'):
-            return self.userEntry.get_text()
+        if (response == Gtk.ResponseType.YES) and (self.type == 'entry'):
+            user_text = self.userEntry.get_text()
+            self.dialog.destroy()
+            return user_text
         else:
+            self.dialog.destroy()
             return response
 
 
-## End dialog
-def MessageDialogEnd(warnC, errC, logFile, title, msg):
-    if (errC != 0):
-        msg += "\nError = " + str(errC)
-        msg += "\nLog file = " + str(logFile)
-        MessageDialog(type_='error', title=title, message=msg).run()
+# End dialog
+def MessageDialogEnd(error, log_file, title, msg1, msg2):
+    if error:
+        msg2 += "\nLog file = " + str(log_file)
+        MessageDialog(dialog_type='error', title=title, message1=msg1, message2=msg2).run()
     else:
-        if (warnC != 0):
-            msg += "\nWarning = " + str(warnC)
-        msg += "\n\nLog file = " + str(logFile)
-        MessageDialog(type_='info', title=title, message=msg).run()
+        msg2 += "\nLog file = " + str(log_file)
+        MessageDialog(dialog_type='info', title=title, message1=msg1, message2=msg2).run()
 
-###############################################
+# 'application' code
+# print(MessageDialog('error', "Error !", "Le programme a planté", "comme une grosse ...").run())
+# print(MessageDialog('info', "Info", "C'est une information importante").run())
+# print(MessageDialog('question', "Question ?", "Irais-tu sur la lune si tu le pouvais ?", "une petite aide : pourquoi pas...").run())
+# print(MessageDialog('entry', "Entry", "Question importante", "Donne moi un chiffre").run())
